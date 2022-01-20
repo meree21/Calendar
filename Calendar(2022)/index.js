14637e4df24690bb0175c24dd5028c6bb39f0b76
meree21 <67742880+meree21@users.noreply.github.com>
2022-01-20 13:25:36 +0900

    Update index.js

--- Calendar(2022)/index.js
+++ Calendar(2022)/index.js
@@ -1,6 +1,35 @@
 const date = new Date();
+const preDates = [];
+const thisDates = [];
+const nextDates = [];
 
-const viewYear = date.getFullYear(); // 년도 호출
-const viewMonth = date.getMonth();  // 월 호출
+// 년도 호출
+const viewYear = date.getFullYear(); 
 
-document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`; //querySelector로 .year-month의 텍스트를 변경 
+// 월 호출
+const viewMonth = date.getMonth();  
+
+// 저번달의 마지막 일, 요일
+const prevLast = new Date(viewYear, viewMonth, 0);
+
+// 이번달 마지막 일, 요일
+const thisLast = new Date(viewYear, viewMonth +1, 0);
+
+// 저번달 마지막날짜(PLDate), 마지막 요일(PLDay)
+const PLDate = prevLast.getDate();
+const PLDay = prevLast.getDay();
+
+// 이번달 마지막날짜(PLDate), 마지막 요일(PLDay)
+const TLDate = thisLast.getDate();
+const TLDate = thisLast.getDay();
+
+
+console.log(thisLast)
+
+//querySelector로 .year-month의 텍스트를 변경
+document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+
+
+
+
+
